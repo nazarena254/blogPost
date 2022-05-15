982de166bec81645f7cb3c2bfb01abd194bb7201
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-15 12:29:30 +0300

    create app configurations, initialize flask extensions and register blueprints

--- manage.py
+++ manage.py
@@ -6,23 +6,23 @@ from app.models import User, Post, Comment, PostLike
 
 #change from 'development to production' when deploying to heroku
 #Creating app instance
-app=create('development')
+app=create_app('development')
 
 manager=Manager(app)
 manager.add_command('server',Server)
 
-migration-Migration(app,db)
-migration-add_command('db',MigrateCommand)
+migrate=Migrate(app,db)
+migrate.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Comment=Comment, PostLike=Postlike)
+    return dict(app=app, db=db, User=User, Comment=Comment, PostLike=PostLike)
     
 @manager.command
 def test():
     import unittest
     test=unittest.TestLoader().discover("tests")
-    unittest.TextTestRunner(verbosity = 2).run(tests)
+    unittest.TextTestRunner(verbosity = 2).run(test)
     
 if __name__=='__main__':
     app.config['SECRET_KEY'] = 'renahwambo' #add secret key configaration
