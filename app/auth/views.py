d7499503cdbfd3336b21635ebcd8f250a4201f3e
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-17 14:48:16 +0300

    import wtf in login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,19 +9,14 @@ from .. import db
 def register():
     signup_form = SignUpForm()
     if signup_form.validate_on_submit():
-        user = User(first_name = signup_form.first_name.data,
-                    last_name = signup_form.last_name.data,  
-                    username = signup_form.username.data, 
-                    email = signup_form.email.data,
-                    password = signup_form.password.data)
+        user = User(first_name = signup_form.first_name.data,last_name = signup_form.last_name.data,  username = signup_form.username.data, email = signup_form.email.data, password = signup_form.password.data)
         db.session.add(user)
         db.session.commit()
 
         return redirect(url_for("auth.login"))
+        # return render_template('auth/login.html')
     title = "Sign Up to NazzBlog"
-    return render_template("auth/signup.html", 
-                            signup_form = signup_form,
-                            title = title)
+    return render_template("auth/signup.html", signup_form = signup_form,title = title)
 
 @auth.route("/login", methods = ["GET", "POST"])
 def login():
@@ -35,9 +30,7 @@ def login():
         flash("Invalid Username or Password")
     
     title = "Login to NazzBlog"
-    return render_template("auth/login.html",
-                            login_form = login_form,
-                            title = title)
+    return render_template("auth/login.html", login_form = login_form, title = title)
 
 @auth.route("/logout")
 @login_required
--- app/models.py
+++ app/models.py
@@ -3,6 +3,10 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
 from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
