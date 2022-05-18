3347afba5eabaef4bf5e9bfd7738369b6785fa0a
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-18 17:02:51 +0300

    add test for Comment

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -1,22 +1,14 @@
 import unittest
-from app.models import Post, User, Comment
+from app.models import Comment
 
 class TestPost(unittest.TestCase):
     
     def setUp(self):
-        self.user_naz = User(first_name = "Nazarena",
-                                last_name = "Wambura",
-                                username = "nazarena",
-                                password = "naz123",
-                                email = "mnazwambura@mail.com")
-        self.new_post = Post(post_title = "Tech",
-                            post_content = "practice for mastery",
-                            user_id = self.user_Collins.id)
-        self.new_comment = Comment(comment = "great one",
+        
+        self.new_comment = Comment(comment = "so inspiring",
                                     post_id = self.new_post.id,
                                     user_id = self.user_Collins.id)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.user_naz, User))
-        self.assertTrue(isinstance(self.new_post, Post))
+        
         self.assertTrue(isinstance(self.new_comment, Comment))
