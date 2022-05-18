be7f0aba8df84372b469bd2f6a6e6095bf7bcebc
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-18 14:04:47 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://nancyngunjiri1:nazarenah123@localhost/blog"
+    
    
     # email configurations
     MAIL_SERVER = "smtp.gmail.com"
